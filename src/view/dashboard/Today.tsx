fe2e6fb26b4c5ce4ee89515eeb0ddfb0c0e38f6d
Esayas <isaiahethi@gmail.com>
2024-06-24 07:23:05 -0300

    second commit

---
+++ src/view/dashboard/Today.tsx
@@ -0,0 +1,104 @@
+import { useEffect } from "react";
+
+import {
+  ArrowDownOutlined,
+  ArrowUpOutlined,
+  SwapOutlined,
+} from "@ant-design/icons";
+import { useQuery } from "@tanstack/react-query";
+import {
+  Card,
+  Flex,
+  List,
+  Skeleton,
+  Statistic,
+  Typography,
+  notification,
+} from "antd";
+
+import { getDashboard } from "../../api/dashboard";
+
+const { Text } = Typography;
+const { useNotification } = notification;
+
+const Today = () => {
+  const [api, contextHolder] = useNotification();
+
+  const { error, isLoading, isSuccess, data } = useQuery({
+    queryKey: ["TodayDashBoard"],
+    queryFn: () => getDashboard(),
+  });
+
+  function showNotification(description: string) {
+    api.error({
+      message: "Error",
+      description,
+    });
+  }
+
+  useEffect(() => {
+    if (error) {
+      showNotification(error.message);
+    }
+  }, [error]);
+  return (
+    <>
+      {contextHolder}
+      <List
+        grid={{
+          gutter: 16,
+          xs: 1,
+          sm: 1,
+          md: 2,
+          lg: 2,
+          xl: 3,
+          xxl: 4,
+        }}
+        loading={isLoading}
+        dataSource={isSuccess ? data.data : [1, 2, 3]}
+        renderItem={(item: {
+          title: string;
+          count: number;
+          change: number;
+        }) => (
+          <List.Item>
+            <Card>
+              <Skeleton
+                loading={isLoading}
+                title={{ width: 200 }}
+                paragraph={{ rows: 2, width: [100, 200] }}
+              >
+                <Statistic title={item.title} value={item.count} />
+                <Flex gap={5} align="center">
+                  {item.change < 0 ? (
+                    <ArrowDownOutlined style={{ color: "#FF4D4F" }} />
+                  ) : item.change > 0 ? (
+                    <ArrowUpOutlined style={{ color: "#52c41a" }} />
+                  ) : (
+                    <SwapOutlined style={{ color: "#FAAD14" }} />
+                  )}
+
+                  <Text
+                    strong
+                    type={
+                      item.change < 0
+                        ? "danger"
+                        : item.change > 0
+                        ? "success"
+                        : "warning"
+                    }
+                  >
+                    {item.change?item.change.toFixed(2):item.change}%
+                  </Text>
+                  <Text type="secondary">vs yesterday</Text>
+                </Flex>
+              </Skeleton>
+            </Card>
+          </List.Item>
+        )}
+      />
+    </>
+  );
+};
+
+export default Today;
---
+++ src/view/dashboard/TopRated.tsx
@@ -0,0 +1,85 @@
+import { useEffect } from "react";
+
+import {
+  Tag,
+  Card,
+  Flex,
+  List,
+  Space,
+  Avatar,
+  Typography,
+  notification,
+} from "antd";
+import { StarFilled } from "@ant-design/icons";
+import { useQuery } from "@tanstack/react-query";
+
+import { getTopRatedProducts } from "../../api/product";
+
+const { Title, Text } = Typography;
+const { useNotification } = notification;
+
+const TopRated = () => {
+  const [api, contextHolder] = useNotification();
+
+  function showNotification(description: string) {
+    api.error({
+      message: "Error",
+      description,
+    });
+  }
+
+  const { error, isLoading, isSuccess, data } = useQuery({
+    queryKey: ["Products", "Top Rated"],
+    queryFn: () => getTopRatedProducts(),
+  });
+
+  useEffect(() => {
+    if (error) {
+      showNotification(error.message);
+    }
+  }, [error]);
+
+  return (
+    <>
+      {contextHolder}
+      <Card style={{ marginBottom: 16, height: "calc(50% - 8px)" }}>
+        <Title level={5}>Top Rated Products</Title>
+        <List
+          loading={isLoading}
+          dataSource={isSuccess ? data.data.products.slice(0, 5) : []}
+          renderItem={(item: {
+            name: string;
+            images: string[];
+            rating: number;
+          }) => (
+            <List.Item>
+              <Flex justify="space-between" align="center" flex={1}>
+                <Space>
+                  <Avatar
+                    src={`${import.meta.env.VITE_API_BACKEND_URL}${
+                      item.images[0]
+                    }`}
+                    shape="square"
+                  />
+                  <Text>{item.name}</Text>
+                </Space>
+                <Tag
+                  style={{
+                    padding: "5px 10px",
+                    borderRadius: 20,
+                  }}
+                >
+                  <StarFilled style={{ color: "#FADB14" }} />
+                  {item.rating.toFixed(2)}
+                  Stars
+                </Tag>
+              </Flex>
+            </List.Item>
+          )}
+        />
+      </Card>
+    </>
+  );
+};
+
+export default TopRated;
